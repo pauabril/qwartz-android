9501e15323d1cea0202185a1ea81fc8c03e3ff19
Pau Abril <pauabril@duck.com>
2026-02-18 18:56:43 +0100

    feat: add card component and update list view

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,18 +1,40 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed } from "vue";
+
+import { Card, CardContent } from "@/components/ui/card";
 // import { useI18n } from "vue-i18n";
 
 // const { t, d } = useI18n();
 
 const items = ref<Array<{ id: number; name: string }>>([
-  { id: 1, name: 'Item One' },
-  { id: 2, name: 'Item Two' },
-  { id: 3, name: 'Item Three' },
-])
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+]);
 </script>
 
 <template>
-  <div v-for="item in items" :key="item.id">
-    {{ item.name }}
+  <div class="flex flex-col gap-4 p-2">
+    <Card v-for="item in items" :key="item.id">
+      <CardContent>
+        <h1 class="text-lg font-bold">{{ item.name }}</h1>
+        <p>This is a card component.</p>
+      </CardContent>
+    </Card>
   </div>
 </template>
