f40b68673d0dc4037587af8faf7fefdeb04145ba
Pau Abril <pauabril@duck.com>
2026-02-18 20:28:57 +0100

    feat: centralise NavBar

---
+++ src/components/nav-bar/NavBar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+
+import { Icon } from "@iconify/vue";
+
+import { Button } from "@/components/ui/button";
+
+const route = useRoute();
+
+interface NavBarButton {
+  name: string;
+  label: string;
+  icon: string;
+}
+
+interface Props {
+  buttons: NavBarButton[];
+  orientation?: "horizontal" | "vertical"; // TODO: implement vertical orientation
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  orientation: "horizontal",
+});
+
+const isNavRoute = computed(() => ["calendar", "list", "insights"].includes(route.name as string));
+
+const shouldExpand = (btnName: string) => !isNavRoute.value || route.name === btnName;
+</script>
+
+<template>
+  <nav class="bg-white border-t w-full h-16 px-4 py-2 flex justify-around gap-2 mt-auto">
+    <Button
+      v-for="btn in props.buttons"
+      :key="btn.name"
+      :as="RouterLink"
+      :to="{ name: btn.name }"
+      size="lg"
+      :class="shouldExpand(btn.name) ? 'flex-1' : 'shrink-0'"
+      class="transition-all duration-100 overflow-hidden min-w-1/6"
+    >
+      <Icon :icon="btn.icon" class="w-5 h-5 shrink-0" />
+      <Transition
+        enter-active-class="transition-opacity duration-100"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class=""
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <span v-if="shouldExpand(btn.name)" class="ml-2">
+          {{ btn.label }}
+        </span>
+      </Transition>
+    </Button>
+  </nav>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,20 +2,13 @@
 import { ref, computed } from "vue";
 import { RouterView, RouterLink, useRoute } from "vue-router";
 
-import { Button } from "@/components/ui/button";
-import { Icon } from "@iconify/vue";
-
-const route = useRoute();
+import { NavBar } from "@/components/nav-bar";
 
 const navBarButtons = [
   { name: "calendar", label: "Calendar", icon: "tabler:calendar-month" },
   { name: "list", label: "Home", icon: "tabler:home" },
   { name: "insights", label: "Insights", icon: "tabler:chart-line" },
 ];
-
-const isNavRoute = computed(() => ["calendar", "list", "insights"].includes(route.name as string));
-
-const shouldExpand = (btnName: string) => !isNavRoute.value || route.name === btnName;
 </script>
 
 <template>
@@ -23,29 +16,5 @@ const shouldExpand = (btnName: string) => !isNavRoute.value || route.name === bt
     <RouterView />
   </main>
 
-  <nav class="bg-white border-t w-full h-16 px-4 py-2 flex justify-around gap-2 mt-auto">
-    <Button
-      v-for="btn in navBarButtons"
-      :key="btn.name"
-      :as="RouterLink"
-      :to="{ name: btn.name }"
-      size="icon-lg"
-      :class="shouldExpand(btn.name) ? 'flex-grow' : 'flex-shrink-0'"
-      class="transition-all duration-100 overflow-hidden"
-    >
-      <Icon :icon="btn.icon" class="w-5 h-5 flex-shrink-0" />
-      <Transition
-        enter-active-class="transition-opacity duration-100"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class=""
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-      >
-        <span v-if="shouldExpand(btn.name)" class="ml-2">
-          {{ btn.label }}
-        </span>
-      </Transition>
-    </Button>
-  </nav>
+  <NavBar :buttons="navBarButtons" />
 </template>
