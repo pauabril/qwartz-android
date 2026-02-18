8e07f7f290c10861da2f95bb998373834a63e356
Pau Abril <pauabril@duck.com>
2026-02-18 18:56:57 +0100

    feat: update main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { RouterView, RouterLink } from 'vue-router'
+import { ref, computed } from "vue";
+import { RouterView, RouterLink } from "vue-router";
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 // const { t, d } = useI18n();
 </script>
 
 <template>
-  <main>
+  <main class="h-full w-full overflow-scroll">
     <RouterView />
-
-    <nav class="bg-white border-t fixed bottom-0 w-full px-4 py-2 flex justify-around">
-      <Button :as="RouterLink" :to="{ name: 'calendar' }" size="icon">📅</Button>
-      <Button :as="RouterLink" :to="{ name: 'list' }" size="icon">📋</Button>
-      <Button :as="RouterLink" :to="{ name: 'insights' }" size="icon">📊</Button>
-    </nav>
   </main>
+
+  <nav class="bg-white border-t w-full h-16 px-4 py-2 flex justify-around mt-auto">
+    <Button :as="RouterLink" :to="{ name: 'calendar' }" size="icon">📅</Button>
+    <Button :as="RouterLink" :to="{ name: 'list' }" size="icon">📋</Button>
+    <Button :as="RouterLink" :to="{ name: 'insights' }" size="icon">📊</Button>
+  </nav>
 </template>
