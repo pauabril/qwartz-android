7c0cf36b91eb4b3aa6ecf920f3076bef80ab0ce9
pauabril <pauabril@duck.com>
2026-02-22 17:26:21 +0100

    feat: mock swiped cards

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed } from 'vue'
 
-import { RouterLink, useRouter } from "vue-router";
+import { RouterLink, useRouter } from 'vue-router'
 
-import { Icon } from "@iconify/vue";
+import { Icon } from '@iconify/vue'
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -13,7 +13,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card'
 import {
   Drawer,
   DrawerClose,
@@ -23,38 +23,38 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Separator } from "@/components/ui/separator";
+} from '@/components/ui/drawer'
+import { Separator } from '@/components/ui/separator'
 
-const router = useRouter();
+const router = useRouter()
 
 const items = ref<Array<{ id: number; name: string; tags?: string[] }>>([
-  { id: 1, name: "Item One", tags: ["tag1", "tag2"] },
-  { id: 2, name: "Item Two", tags: ["tag3"] },
-  { id: 3, name: "Item Three" },
-  { id: 1, name: "Item One", tags: ["tag1", "tag2"] },
-  { id: 2, name: "Item Two", tags: ["tag3"] },
-  { id: 3, name: "Item Three", tags: ["tag4", "tag5"] },
-  { id: 1, name: "Item One", tags: ["tag1", "tag2"] },
-  { id: 2, name: "Item Two", tags: ["tag3"] },
-  { id: 3, name: "Item Three", tags: ["tag4", "tag5"] },
-  { id: 1, name: "Item One", tags: ["tag1", "tag2"] },
-  { id: 2, name: "Item Two" },
-  { id: 3, name: "Item Three", tags: ["tag4", "tag5"] },
-  { id: 1, name: "Item One", tags: ["tag1", "tag2"] },
-  { id: 2, name: "Item Two", tags: ["tag3"] },
-  { id: 3, name: "Item Three" },
-]);
+  { id: 1, name: 'Item One', tags: ['tag1', 'tag2'] },
+  { id: 2, name: 'Item Two', tags: ['tag3'] },
+  { id: 3, name: 'Item Three' },
+  { id: 1, name: 'Item One', tags: ['tag1', 'tag2'] },
+  { id: 2, name: 'Item Two', tags: ['tag3'] },
+  { id: 3, name: 'Item Three', tags: ['tag4', 'tag5'] },
+  { id: 1, name: 'Item One', tags: ['tag1', 'tag2'] },
+  { id: 2, name: 'Item Two', tags: ['tag3'] },
+  { id: 3, name: 'Item Three', tags: ['tag4', 'tag5'] },
+  { id: 1, name: 'Item One', tags: ['tag1', 'tag2'] },
+  { id: 2, name: 'Item Two' },
+  { id: 3, name: 'Item Three', tags: ['tag4', 'tag5'] },
+  { id: 1, name: 'Item One', tags: ['tag1', 'tag2'] },
+  { id: 2, name: 'Item Two', tags: ['tag3'] },
+  { id: 3, name: 'Item Three' },
+])
 
 const timeEntries = ref([
   {
     id: 1,
-    title: "Mock Time Entry",
-    tags: ["work", "project1"],
-    start: new Date("2026-01-01T09:00:00"),
-    end: new Date("2026-01-01T12:34:56"),
+    title: 'Mock Time Entry',
+    tags: ['work', 'project1'],
+    start: new Date('2026-01-01T09:00:00'),
+    end: new Date('2026-01-01T12:34:56'),
   },
-]);
+])
 </script>
 
 <template>
@@ -115,6 +115,72 @@ const timeEntries = ref([
         </div>
       </CardContent>
     </Card>
+
+    <!-- ----------------------------------------------------------------------------------- -->
+    <Separator />
+    <!-- ----------------------------------------------------------------------------------- -->
+
+    <div class="flex flex-row items-center justify-center gap-4">
+      <Card v-for="entry in timeEntries" :key="entry.id">
+        <CardContent class="flex flex-row">
+          <div class="flex flex-col gap-2">
+            <h1 class="text-lg font-bold line-clamp-1">{{ entry.title }}</h1>
+            <div class="flex gap-2 mt-2">
+              <span
+                v-for="tag in entry.tags"
+                :key="tag"
+                class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs"
+              >
+                {{ tag }}
+              </span>
+            </div>
+          </div>
+          <div class="flex items-center justify-end gap-2 ml-auto">
+            {{ new Date(entry.end.getTime() - entry.start.getTime()).toISOString().substr(11, 8) }}
+            <Button variant="ghost" size="icon-lg"
+              ><Icon icon="tabler:dots" class="size-6"
+            /></Button>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Button variant="link" size="icon-lg" class="text-destructive">
+        <Icon icon="tabler:trash" class="size-6" />
+      </Button>
+    </div>
+
+    <!-- ----------------------------------------------------------------------------------- -->
+    <Separator />
+    <!-- ----------------------------------------------------------------------------------- -->
+
+    <div class="flex flex-row items-center justify-center gap-4">
+      <Button variant="link" size="icon-lg">
+        <Icon icon="tabler:copy" class="size-6" />
+      </Button>
+
+      <Card v-for="entry in timeEntries" :key="entry.id">
+        <CardContent class="flex flex-row">
+          <div class="flex flex-col gap-2">
+            <h1 class="text-lg font-bold line-clamp-1">{{ entry.title }}</h1>
+            <div class="flex gap-2 mt-2">
+              <span
+                v-for="tag in entry.tags"
+                :key="tag"
+                class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs"
+              >
+                {{ tag }}
+              </span>
+            </div>
+          </div>
+          <div class="flex items-center justify-end gap-2 ml-auto">
+            {{ new Date(entry.end.getTime() - entry.start.getTime()).toISOString().substr(11, 8) }}
+            <Button variant="ghost" size="icon-lg"
+              ><Icon icon="tabler:dots" class="size-6"
+            /></Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
 
     <!-- ----------------------------------------------------------------------------------- -->
     <Separator />
